b9e4c8d467f038b76114a319177b84cd0c47e409
Paulo Neves <ptsneves@gmail.com>
2019-06-22 19:20:23 +0200

    Added Map Dashboard.
    Added a Map dahsboard where google maps are the backend to show
    markers of where pictures were taken. Right now not much
    happens in this Map dashboard besides centering around Portugal
     showing the markers of the pictures and some basic information
    of where the pictures were taken. This is very rudimental and
    proof of concept as the structure of the devices is not yet
    taken into account and no thumbnails are shown also.

--- Areas/DHub/Controllers/DevicesController.cs
+++ Areas/DHub/Controllers/DevicesController.cs
@@ -158,6 +158,10 @@ namespace DroHub.Areas.DHub.Controllers
             return View();
         }
 
+        public IActionResult Map() {
+            return View();
+        }
+
         // POST: DroHub/Devices/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
