44a401110f632648f54e758bee62505e2cee143c
Paulo Neves <ptsneves@gmail.com>
2019-06-22 19:12:10 +0200

    sidemenu-drohib.js: Made it more robust and beautiful
    Made the side menu tree menu more robust in the parsing of
    the json. This is required because we want to not destroy
    the rendering of the whole page just because an error in the
    side menu happened, due to for example there being no devices
    associated.
    
    Also we added a very nice plug icon to each of the devices :)

--- wwwroot/js/sidemenu-drohub.js
+++ wwwroot/js/sidemenu-drohub.js
@@ -1,10 +1,15 @@
 function generateRows(device) {
 }
-function populateDevices(get_device_list_url, gallery_url) {
-    $.getJSON(get_device_list_url, function (device_list) {
-        $.each(device_list, function (i, device) {
-            device_gallery_url = gallery_url + '/' + device.Id +
-            $('#devices-list').append('<li><a href=' + device_gallery_url + '> <i class="fa fa-circle-o"></i>' + device.Name + ' </a ></li > ')
+function populateDevices(get_device_list_url, device_gallery_url) {
+    $.getJSON(get_device_list_url)
+        .done(function (device_list) {
+            $.each(device_list, function (i, device) {
+                device_gallery_url = gallery_url + '/' + device.id
+                li = '<li><a href=' + device_gallery_url + '> <i class="fa fa-plug"></i>' + device.name + ' </a ></li > '
+                    $('#devices-list').append(li)
+            })
         })
-    })
+        .fail(function () {
+            console.log("dsadd")
+        })
 }
