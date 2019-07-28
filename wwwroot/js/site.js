4d0a0211eb66ca485eb7d7868ea113248b40bcd0
Paulo Neves <ptsneves@gmail.com>
2019-07-28 22:01:33 +0200

    site.js: Added followActionAndDoNothing javascript helper.
    This helper is usefull when we want a user to press a button
    that will trigger an action but we do not want to updat the
    page to the user. No usage of this function yet.
    
    Added site.js to the _Main.cshtml

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function followActionAndDoNothing(action_url) {
+    $.getJSON(action_url)
+}
