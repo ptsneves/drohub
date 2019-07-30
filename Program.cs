30975acdf16d3ae01250d3d055c7fe1da2079149
Paulo Neves <ptsneves@gmail.com>
2019-07-30 20:15:19 +0200

    Added support for Serilog logging provider.
    The serilog logging provider is a bit more powerfull than the default
    logging provider. One of the most important things is that it allows for
    different pre-existing sinks, as well as storage of properties alongside
    the logging message itself.
    
    Also added the reference for the mssql logging sink, although no such
    sink is in use at the time of this commit.
    
    In this commit we just set up a basic console sink and the only message logged
    is in the Program start. Future functionality will be added.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using System;
+using Serilog.Events;
+using Serilog;
 
 namespace DroHub
 {
@@ -8,7 +11,25 @@ namespace DroHub
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message,30:lj} {Properties:j}{NewLine}{Exception}")
+                .CreateLogger();
+
+            try {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -16,6 +37,7 @@ namespace DroHub
             .UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
             .UseIISIntegration()
-            .UseStartup<Startup>();
+            .UseStartup<Startup>()
+            .UseSerilog();
     }
 }
