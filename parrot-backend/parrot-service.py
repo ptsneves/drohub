31ac1ea0424223bd5efee9ec6ae08a7362019c01
Paulo Neves <ptsneves@gmail.com>
2019-08-18 16:36:18 +0200

    parrot-service: Added position stream.
    Current commit adds the possibility of having a stream that consumes
    a queue with position from the drone

--- parrot-backend/parrot-service.py
+++ parrot-backend/parrot-service.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 from concurrent import futures
+from collections import deque
 import logging
 import time
+import threading
 
 import grpc
 import DrohubGRPCProto.python.drohub_pb2_grpc as drohub_pb2_grpc
@@ -27,16 +29,31 @@ def check_drone_connected(f):
 class DroneRPC(drohub_pb2_grpc.DroneServicer):
     def __init__(self):
         super().__init__()
+        self.positions = deque(maxlen=3)
+        self.lk_positions = threading.Lock()
+        self.cv_positions_consumer = threading.Condition(self.lk_positions)
         while True:
             self.drone = olympe.Drone("10.202.0.1", callbacks = [self.cb1])
-            print("Connected")
             if self.drone.connection() == True:
                 break
 
     def cb1(self, message):
         if message.Full_Name == "Ardrone3_PilotingState_PositionChanged":
-            print(message.Full_Name, message.state())
-            print()
+            self.positions.append(message.copy())
+            with self.cv_positions_consumer:
+                self.cv_positions_consumer.notify_all()
+
+
+    @check_drone_connected
+    def getPosition(self, request, context):
+        while True:
+            with self.cv_positions_consumer:
+                self.cv_positions_consumer.wait()
+                positions_copy = self.positions
+                while len((positions_copy)) > 0:
+                    position = positions_copy.popleft()
+                    yield drohub_pb2.DronePosition(latitude=position.state()['latitude'], longitude=position.state()['longitude'],
+                        altitude=position.state()['altitude'])
 
     @check_drone_connected
     def doTakeoff(self, request, context):
