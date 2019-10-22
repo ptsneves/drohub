c4dadbbc0cb9513e947a20714295480d96197254
Paulo Neves <ptsneves@gmail.com>
2019-10-22 17:17:27 +0200

    DroHub: Upgrade dotnet framework to 2.2.
    Before we were in 2.1. This ways we hope to soon be able to migrate to
    3.0, as it is already released

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,7 @@ namespace DroHub
             services.AddHttpClient<JanusService>();
 
             services.AddSignalR();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
