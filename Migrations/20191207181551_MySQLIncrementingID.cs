972ffb1e11513f75320157326233daae05f9176c
Paulo Neves <ptsneves@gmail.com>
2019-12-07 19:42:01 +0100

    mysql: Created a custom migration to fix id increment.
    When we created the Devices migration there was an annotation for
    Id autoincrement but this was MSSQL specific. As we added MySQL we
    also needed to add an analogous annotation otherwise we had collisions
    everytime we tried to add more than one device.

---
+++ Migrations/20191207181551_MySQLIncrementingID.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+
+namespace DroHub.Data.Migrations
+{
+    public partial class MySQLIncrementingID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET foreign_key_checks = 0");
+            var pomeloMigrationNoteCore2Bug = "ALTER TABLE {0} MODIFY COLUMN {1} int auto_increment";
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Devices",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+            migrationBuilder.Sql(String.Format(pomeloMigrationNoteCore2Bug, "Devices", "Id"));
+            migrationBuilder.Sql("SET foreign_key_checks = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //I do not know how to revert this
+        }
+    }
+}
