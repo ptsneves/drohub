94a59b79c0333a54422aafb1aeb123bd8f5c632e
Paulo Neves <ptsneves@gmail.com>
2019-12-13 14:02:19 +0100

    modals: Added ability to render modals from partial views.
    This ability is crucial to have a simpler website navigation
    while keeping the organization facilities that dotnet provides
    us.
    
    An important detail of how this works is that when the modelstate
    fails the modal does not exit, instead showing the model state errors.
    All this with minimal javascript. Very happy :)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,3 +5,33 @@
 function followActionAndDoNothing(action_url) {
     $.getJSON(action_url)
 }
+$(function () {
+    var placeholderElement = $('#modal-placeholder');
+    function makeModal (event) {
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            placeholderElement.html(data);
+            placeholderElement.find('.modal').modal({ focus: true, show: true, keyboard: true });
+        });
+    }
+    $('a[data-toggle="ajax-modal"]').click(makeModal);
+    $('button[data-toggle="ajax-modal"]').click(makeModal);
+
+    placeholderElement.on('click', '[data-save="modal"]', function (event) {
+        event.preventDefault();
+
+        var form = $(this).parents('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var dataToSend = form.serialize();
+
+        $.post(actionUrl, dataToSend).done(function (data) {
+            var newBody = $('.modal-body', data);
+            placeholderElement.find('.modal-body').replaceWith(newBody);
+
+            var isValid = newBody.find('[name="IsValid"]').val() == 'True';
+            if (isValid) {
+                placeholderElement.find('.modal').modal('hide');
+            }
+        });
+    });
+});
