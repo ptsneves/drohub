4d770f4db16bf67102e2e96c4d50d6228169b049
Paulo Neves <ptsneves@gmail.com>
2019-12-27 14:05:52 +0100

    Account: Renamed typo AdminPannel to AdminPanel.
    Made a typo and now we correct it. Some files were also
    wrongly named.

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -21,7 +21,7 @@ namespace DroHub.Areas.Identity.Pages.Account.Manage
         public static string PersonalData => "PersonalData";
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
-        public static string AdminPannel => "AdminPannel";
+        public static string AdminPanel => "AdminPanel";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
@@ -36,7 +36,7 @@ namespace DroHub.Areas.Identity.Pages.Account.Manage
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
-        public static string AdminPannelUserNavClass(ViewContext viewContext) => PageNavClass(viewContext, AdminPannel);
+        public static string AdminPanelUserNavClass(ViewContext viewContext) => PageNavClass(viewContext, AdminPanel);
 
         public static string PageNavClass(ViewContext viewContext, string page)
         {
