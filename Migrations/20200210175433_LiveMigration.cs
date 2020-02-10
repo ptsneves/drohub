7c10e0a027d2b85b6a020c83b69e709f63aaa11f
Paulo Neves <ptsneves@gmail.com>
2020-02-10 18:55:50 +0100

    migrations: Adaptations to the videoroom concept.

---
+++ Migrations/20200210175433_LiveMigration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DroHub.Data.Migrations
+{
+    public partial class LiveMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HumanMessage",
+                table: "DroneVideoStatesResults");
+
+            migrationBuilder.DropColumn(
+                name: "RtpUrl",
+                table: "DroneVideoStatesResults");
+
+            migrationBuilder.DropColumn(
+                name: "LiveVideoFMTProfile",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "LiveVideoPt",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "LiveVideoRTPMap",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "LiveVideoRTPUrl",
+                table: "Devices");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HumanMessage",
+                table: "DroneVideoStatesResults",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RtpUrl",
+                table: "DroneVideoStatesResults",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LiveVideoFMTProfile",
+                table: "Devices",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LiveVideoPt",
+                table: "Devices",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LiveVideoRTPMap",
+                table: "Devices",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LiveVideoRTPUrl",
+                table: "Devices",
+                nullable: true);
+        }
+    }
+}
