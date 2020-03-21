727fff597aeae0abaf945927f1569d2b5710d12b
Paulo Neves <ptsneves@gmail.com>
2020-03-21 21:04:46 +0100

    refactor(DroHubUser): Redeuce rule duplication
    The policy rules were all independent instead of chained and this led to
    a lot of duplication. Also as the rule list increased, it was becoming
    hard to understand what rules were common or not

--- Areas/Identity/Data/DroHubUser.cs
+++ Areas/Identity/Data/DroHubUser.cs
@@ -1,9 +1,8 @@
-using DroHub.Areas.DHub.Models;
-using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
+using DroHub.Areas.DHub.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace DroHub.Areas.Identity.Data
 {
@@ -18,59 +17,42 @@ namespace DroHub.Areas.Identity.Data
         public const string PILOT_POLICY_CLAIM = "ActingPilot";
         public const string GUEST_POLICY_CLAIM = "ActingGuest";
 
+        private static readonly List<Claim> GuestPolicyClaims = new List<Claim>() {
+            new Claim(GUEST_POLICY_CLAIM, CLAIM_VALID_VALUE),
+            new Claim(Device.CAN_PERFORM_CAMERA_ACTION, Device.CLAIM_VALID_VALUE),
+        };
+
+        private static readonly List<Claim> PilotPolicyClaims = new List<Claim>(GuestPolicyClaims) {
+            new Claim(PILOT_POLICY_CLAIM, CLAIM_VALID_VALUE),
+            new Claim(Device.CAN_ADD_CLAIM, Device.CLAIM_VALID_VALUE),
+            new Claim(Device.CAN_MODIFY_CLAIM, Device.CLAIM_VALID_VALUE),
+            new Claim(Device.CAN_PERFORM_FLIGHT_ACTIONS, Device.CLAIM_VALID_VALUE),
+        };
+
+        private static readonly List<Claim> OwnerPolicyClaims = new List<Claim>(PilotPolicyClaims) {
+            new Claim(OWNER_POLICY_CLAIM, CLAIM_VALID_VALUE),
+            new Claim(Subscription.CAN_SEE_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
+            new Claim(Subscription.CAN_EDIT_USERS_IN_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
+        };
+
+        private static readonly List<Claim> SubscriberPolicyClaims = new List<Claim>(OwnerPolicyClaims) {
+            new Claim(SUBSCRIBER_POLICY_CLAIM, CLAIM_VALID_VALUE),
+        };
+
+        private static readonly List<Claim> AdminPolicyClaims = new List<Claim>(SubscriberPolicyClaims) {
+            new Claim(ADMIN_POLICY_CLAIM, CLAIM_VALID_VALUE),
+            new Claim(Subscription.CAN_MODIFY_CLAIM, Subscription.CLAIM_VALID_VALUE),
+            new Claim(Subscription.CAN_ADD_CLAIM, Subscription.CLAIM_VALID_VALUE),
+            new Claim(Subscription.CAN_SEE_NOT_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
+        };
+
         public static readonly Dictionary<string, List<Claim>> UserClaims = new Dictionary<string, List<Claim>>
         {
-            {
-                ADMIN_POLICY_CLAIM, new List<Claim>()
-                {
-                    new Claim(ADMIN_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(SUBSCRIBER_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(OWNER_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(PILOT_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(GUEST_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_MODIFY_CLAIM, Subscription.CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_ADD_CLAIM, Subscription.CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_SEE_NOT_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_SEE_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_EDIT_USERS_IN_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
-                }
-            },
-
-            {
-                SUBSCRIBER_POLICY_CLAIM, new List<Claim>()
-                {
-                    new Claim(SUBSCRIBER_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(OWNER_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(PILOT_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(GUEST_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_SEE_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE),
-                    new Claim(Subscription.CAN_EDIT_USERS_IN_OWN_SUBSCRIPTION, Subscription.CLAIM_VALID_VALUE)
-                }
-            },
-
-            {
-                OWNER_POLICY_CLAIM, new List<Claim>()
-                {
-                    new Claim(OWNER_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(PILOT_POLICY_CLAIM, CLAIM_VALID_VALUE),
-                    new Claim(GUEST_POLICY_CLAIM, CLAIM_VALID_VALUE)
-                }
-            },
-
-            {
-                PILOT_POLICY_CLAIM,  new List<Claim>()
-                {
-                    new Claim(PILOT_POLICY_CLAIM, CLAIM_VALID_VALUE)
-                }
-            },
-
-            {
-                GUEST_POLICY_CLAIM, new List<Claim>()
-                {
-                    new Claim(GUEST_POLICY_CLAIM, CLAIM_VALID_VALUE)
-                }
-            }
-
+            { ADMIN_POLICY_CLAIM, AdminPolicyClaims },
+            { SUBSCRIBER_POLICY_CLAIM, SubscriberPolicyClaims },
+            { OWNER_POLICY_CLAIM, OwnerPolicyClaims },
+            { PILOT_POLICY_CLAIM, PilotPolicyClaims },
+            { GUEST_POLICY_CLAIM, GuestPolicyClaims }
         };
 
         public string BaseActingType { get; set; }
