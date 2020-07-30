c214d619b51d06782d31be12f6aa3055b1096cae
Paulo Neves <ptsneves@gmail.com>
2020-07-30 19:41:11 +0200

    feat(Login.cshtml): Record login time.
    We are now showing in the AccountManager the last login
    time so we need to update it when a user logs in.

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -74,10 +74,12 @@ namespace DroHub.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
-
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User {@user} logged in.", Input.Email);
+                    var user = await _signInManager.UserManager.FindByNameAsync(Input.Email);
+                    user.LastLogin = DateTime.UtcNow;
+                    var r = await _signInManager.UserManager.UpdateAsync(user);
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
