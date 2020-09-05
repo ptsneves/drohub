2d9e6303a244d018eee99b5f2a948e2323ad32a4
Paulo Neves <ptsneves@gmail.com>
2020-09-05 20:53:40 +0200

    SnackBarInfoDisplay: Set the snackbar text to be centered.

--- parrot-backend/android/app/src/main/java/com/drohub/SnackBarInfoDisplay.java
+++ parrot-backend/android/app/src/main/java/com/drohub/SnackBarInfoDisplay.java
@@ -1,6 +1,9 @@
 package com.drohub;
 
+import android.os.Build;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class SnackBarInfoDisplay extends InfoDisplayBase {
@@ -8,6 +11,12 @@ public class SnackBarInfoDisplay extends InfoDisplayBase {
     public SnackBarInfoDisplay(View root_view, long refresh_period_ms) {
         super(refresh_period_ms);
         _snackbar = Snackbar.make(root_view, "", Snackbar.LENGTH_INDEFINITE);
+        View view = _snackbar.getView();
+        TextView tv = view.findViewById(com.google.android.material.R.id.snackbar_text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        else
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
     }
 
     @Override
