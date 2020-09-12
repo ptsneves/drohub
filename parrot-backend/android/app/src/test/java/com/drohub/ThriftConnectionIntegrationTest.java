758c37057cd6244ebd4e5e8c296c86049785efb9
Paulo Neves <ptsneves@gmail.com>
2020-09-12 13:54:42 +0200

    ThriftConnectionIntegrationTest: Disable test until framwork supports.
    Right now we have no way of making integration tests of platform
    and app code. This can change in the future and when the frameworks
    supports it we can orchestrate the app integration tests with the
    platform.

--- parrot-backend/android/app/src/test/java/com/drohub/ThriftConnectionIntegrationTest.java
+++ parrot-backend/android/app/src/test/java/com/drohub/ThriftConnectionIntegrationTest.java
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 public class ThriftConnectionIntegrationTest {
     @Test
     public void SimpleTest() throws InterruptedException, IOException {
-        ThriftConnection c = new ThriftConnection();
-        c.onStart("PI040416BA8H083705", "ws://localhost:5000/ws",
-                "ws://192.168.1.106:8188", null);
-        Thread.sleep(10000);
-        c.onStop();
+//        ThriftConnection c = new ThriftConnection();
+//        c.onStart("PI040416BA8H083705", "ws://localhost:5000/ws",
+//                "ws://192.168.1.106:8188", null);
+//        Thread.sleep(10000);
+//        c.onStop();
     }
 }
