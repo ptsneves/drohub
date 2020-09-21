82ac9bec58d8fa876176c3af7c06d24cd75388de
Paulo Neves <ptsneves@gmail.com>
2020-09-21 23:04:08 +0200

    docker-letsencrypt: domain and gandi user are env variables.
    Up to now the letsencrypt docker had the credentials
    hardcoded at build time. Now we have the gandi credentials
    as well as domain and www.domain passed as environmental
    variables. If these env variables are not set the container
    will fail.
    
    The credentials are still recorded in git, but the truth is they
    will not match the ones in production from now on.

--- docker/docker-letsencrypt/letsencrypt.sh
+++ docker/docker-letsencrypt/letsencrypt.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
+if [ -z ${DOMAIN} ]; then
+    >&2 echo "Error: DOMAIN env variable not set. Failing."
+    exit 1
+fi
+
+if [ -z ${GANDI_USER} ]; then
+    >&2 echo "Error: GANDI_USER env variable not set. Failing."
+    exit 1
+fi
+
+if [ -z ${GANDI_KEY} ]; then
+    >&2 echo "Error: GANDI_KEY env variable not set. Failing."
+    exit 1
+fi
+
+umask 077
+echo "certbot_plugin_gandi:dns_api_key=${GANDI_KEY}" > gandi.ini
+
 while true; do
     certbot certonly \
         -a certbot-plugin-gandi:dns \
         --certbot-plugin-gandi:dns-credentials gandi.ini \
-        -d drohub.xyz \
-        -d www.drohub.xyz \
+        -d ${DOMAIN} \
+        -d www.${DOMAIN} \
         --noninteractive \
         --agree-tos \
         --expand \
-        -m ptsneves@gmail.com && \
+        -m ${GANDI_USER} && \
         sleep 360000 # 10 hours
 done;
