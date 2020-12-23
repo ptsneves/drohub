83ec6fed22671296bb852866ad9a5c326eadb109
Paulo Neves <ptsneves@gmail.com>
2020-12-23 15:36:45 +0100

    GalleryItemSelect: New graphics for gallery item selection.
    Before we had this ugly ass blue frame signaling the selection.
    Now we have the designer's icons active on the media whenever
    there is an active selection.

--- drohub-vue/src/Gallery/components/GalleryItemSelect.vue
+++ drohub-vue/src/Gallery/components/GalleryItemSelect.vue
@@ -1,15 +1,31 @@
 <template>
-    <div
-        v-on:click="toggleSelection"
-        v-show="isEnabled"
-        class="selectable-box"
-        :class="{'selected-item': isItemSelected}"
-    ></div>
+    <div class="gallery-item-select"
+         v-show="isEnabled"
+    >
+        <inline-svg
+            v-bind:src="require('../../../../wwwroot/images/assets/timeline-select-selected.svg')"
+            v-on:click="toggleSelection"
+            v-show="isItemSelected"
+            class="item-select"
+            v-bind:transform-source="transform"
+        ></inline-svg>
+        <inline-svg
+            v-bind:src="require('../../../../wwwroot/images/assets/timeline-select-unselected.svg')"
+            v-on:click="toggleSelection"
+            v-show="!isItemSelected"
+            class="item-select"
+            v-bind:transform-source="transform"
+        ></inline-svg>
+    </div>
 </template>
 
 <script>
+    import InlineSvg from 'vue-inline-svg';
     export default {
         name: "GalleryItemSelect",
+        components: {
+            InlineSvg,
+        },
         props: {
             itemId: {
                 type: String,
@@ -32,24 +48,38 @@
                 };
                 this.$emit('update:selection-event', state);
             },
+            transform(svg) {
+                let ids = svg.querySelectorAll('[id]');
+
+                Array.prototype.forEach.call( ids, function( el, i ) {
+                    let classes = el.id;
+                    if (el.class !== undefined)
+                        classes = el.class + " " + classes;
+                    el.removeAttributeNS(null, 'id')
+                    el.setAttributeNS(null, 'class', classes);
+                });
+                return svg;
+            }
         }
     }
 </script>
 
-<style scoped>
-    .selected-item {
-        border: 0.5rem solid #1099ff !important;
-    }
-
-    .selectable-box {
+<style>
+    .item-select {
         background: transparent;
         position: absolute;
         z-index: 9997;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        border-radius: 10px;
-        border: dashed gray;
+        top: 8%;
+        right: 12%;
+        width: 6%;
+        height: 8%;
+    }
+    .Oval {
+        fill: white;
+        opacity: 80%;
+    }
+    .Shape {
+        fill: white;
+        opacity: 80%;
     }
 </style>
